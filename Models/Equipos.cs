37036966668534f47dae041d199df5367b49ad58
FRANCISCO DOMINGUEZ NAVARRO <47344051@ort>
2023-05-11 15:54:30 -0300

    modificacion 11/05

---
+++ Models/Equipos.cs
@@ -0,0 +1,9 @@
+ public static class Equipos()
+    {
+        public List<string> ListaEquipos = new List<string>();
+        public List<string> ListaMedios = new List<string>();
+        public List<string> ListaPantalones = new List<string>();
+        public List<string> ListaRemeras = new List<string>();
+
+        public Dictionary<string, Indumentaria> EquiposIndumentaria = new Dictionary<string, Indumentaria>();
+    }
