4eb9142923c7c620a815114aed4b3862e7a82af7
FRANCISCO DOMINGUEZ NAVARRO <47344051@ort>
2023-05-11 15:21:37 -0300

    inicio mvc

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace tp4indumentari.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
